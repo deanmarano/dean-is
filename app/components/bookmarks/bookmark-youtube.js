968378fdd5cf86f56a460ccc5991154434bc6e4a
Dean Marano <dean.marano@gmail.com>
2018-11-07 19:40:35 -0600

    Styling for bookmarks.

--- app/components/bookmarks/bookmark-youtube.js
+++ app/components/bookmarks/bookmark-youtube.js
@@ -1,5 +1,8 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['entity']
+  onInserted() {
+    debugger;
+  },
+  classNames: ['entity', 'youtube']
 });
