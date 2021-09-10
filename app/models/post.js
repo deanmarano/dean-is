80b5be3118fbf00df6128de902a52395a5d616f4
Dean Marano <dean.marano@gmail.com>
2021-09-10 16:56:09 -0400

    Do not display gender post.

--- app/models/post.js
+++ app/models/post.js
@@ -1,24 +1,26 @@
-export default [{
-        filename: '/posts/gender-in-web-forms.md',
-        url: 'thinking-about-gender-in-web-forms',
-        titlePrefix: 'thinking about ',
-        title: 'Gender in Web Forms',
-        date: ' 2016-09-29'
-      }, {
-        filename: '/posts/data-types.md',
-        url: 'looking-at-data-types',
-        titlePrefix: 'looking at ',
-        title: 'Basic Data Types',
-        date: ' 2016-09-20'
-      }, {
-        filename: '/posts/development-tips.md',
-        url: 'collecting-development-tips',
-        title: 'Development Tips',
-        titlePrefix: 'collecting ',
-        date: ' Ongoing'
-      }, {
-        externalHost: 'Medium',
-        url: 'https://medium.com/espark-engineering-blog/testing-your-jekyll-site-the-ruby-way-ccfa386a8318',
-        title: 'Testing Jekyll, The Ruby Way',
-        date: ' 2016-07-07'
-      }];
+export default [
+  {
+    //filename: '/posts/gender-in-web-forms.md',
+    //url: 'thinking-about-gender-in-web-forms',
+    //titlePrefix: 'thinking about ',
+    //title: 'Gender in Web Forms',
+    //date: ' 2016-09-29'
+  //}, {
+    filename: '/posts/data-types.md',
+    url: 'looking-at-data-types',
+    titlePrefix: 'looking at ',
+    title: 'Basic Data Types',
+    date: ' 2016-09-20'
+  }, {
+    filename: '/posts/development-tips.md',
+    url: 'collecting-development-tips',
+    title: 'Development Tips',
+    titlePrefix: 'collecting ',
+    date: ' Ongoing'
+  }, {
+    externalHost: 'Medium',
+    url: 'https://medium.com/espark-engineering-blog/testing-your-jekyll-site-the-ruby-way-ccfa386a8318',
+    title: 'Testing Jekyll, The Ruby Way',
+    date: ' 2016-07-07'
+  }
+];
